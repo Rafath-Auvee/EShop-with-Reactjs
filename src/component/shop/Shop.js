9bc44e3e584ff0ff4bf6642388662872137f406b
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-03-23 16:38:14 +0600

    useState fixed

--- src/component/shop/Shop.js
+++ src/component/shop/Shop.js
@@ -8,12 +8,12 @@ const Shop = () => {
   useEffect( ()=>{
     fetch('products.json')
     .then(res=>res.json())
-    .then(data=>console.log(data)) 
+    .then(data => setProducts(data)) 
   } , [])
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h1>This is for products</h1>
+        <h1>This is for products {products.length} </h1>
       </div>
       <div className="cart-container">
         <h1>Order Summary</h1> 
