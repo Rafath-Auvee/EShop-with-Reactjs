c365df6472cadf711d43152e8542abb1bf511e69
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-03-23 15:07:56 +0600

    header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-
+// import './App.css';
+import Header from "./component/header/Header";
 function App() {
 
   return (
-    <div className="App">
+    <div>
+      <Header>
 
+      </Header>
     </div>
   );
 }
---
+++ src/component/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/logo2.svg'
+import './Header.css'
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <a href="/shop">Shop</a>
+        <a href="/orders">Orders</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/about">About</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
