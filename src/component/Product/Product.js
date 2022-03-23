86209e559910ce6db45aa3e59e9476bcbb3d9db9
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-03-23 16:59:26 +0600

    products shown

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import "./Product.css"
 const Product = (props) => {
   const {name, seller, ratings, price, img} = props.product
   return (
-    <div>
+    <div className="product">
       <img src={img} alt={name} />
     </div>
   );
