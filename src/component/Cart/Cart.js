829aa4a20aaddf47eb382873bcda3ba3fb6e3775
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-03-24 19:12:40 +0600

    sidebar

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = (props) => {
   return (
-    <div>
+    <div className="cart">
         <h1>Order Summary</h1>
         <p> Total Item {props.cart.length} </p> 
     </div>
