50955245c28b911bdcbc5cbac85c7154181412ec
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-03-25 15:18:43 +0600

    total price functional

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
   const {cart} = props;
+  let total = 0
+
+  for(const product of cart)
+  {
+    total = total + product.price
+  }
+
+  console.log(cart)
   return (
     <div className="cart">
         <h1>Order Summary</h1>
         <p> Selected Item {cart.length} </p>
-        <p>Total Price: </p> 
+        <p>Total Price: ${total}</p> 
         <p>Total Shipping: </p>
         <p>Tax: </p>
         <p>Grand Total: </p>
