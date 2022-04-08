2acc6eabc7a6e6f7bce8b39e07169d2c93ad36ac
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-04-09 02:12:55 +0600

    order page

--- src/component/ReviewItem/ReviewItem.js
+++ src/component/ReviewItem/ReviewItem.js
@@ -1,3 +1,5 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./ReviewItem.css";
 const ReviewItem = (props) => {
@@ -10,13 +12,18 @@ const ReviewItem = (props) => {
       </div>
       <div className="review-item-details-container">
         <div className="review-item-details">
-            <p className="product-name">
-                {name}
+            <p className="product-name" title={name}>
+                {name.length > 20 ? name.slice(0,20)+ '...' : name}
             </p>
-            <p>price <span className="orange-color">{price}</span> </p>
+            <p>price <span className="orange-color">{price}$</span> </p>
+            <p><small>Shipping: {shipping}$</small></p>
+            <p><small>Quantity: {quantity}</small></p>
         </div>
         <div className="delete-container">
-            <button>Delete</button>
+            <button className="delete-button">
+                <FontAwesomeIcon className="delete-icon" icon={faTrash}/>
+            </button>
+
         </div>
       </div>
     </div>
