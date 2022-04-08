5aebafc3cdd245bf1b4dc7a854e4e2dedb842f2a
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-04-08 15:22:26 +0600

    route added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // import './App.css';
 import Header from "./component/header/Header";
 import Shop from "./component/shop/Shop";
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Order from "./component/Order/Order";
+import Inventory from "./component/Inventory/Inventory";
 
+function App() {
   return (
     <div>
       <Header></Header>
-      <Shop/>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Shop />} />
+          <Route path="/Shop" element={<Shop />} />
+          <Route path="/orders" element={<Order />} />
+          <Route path="/inventory" element={<Inventory />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
